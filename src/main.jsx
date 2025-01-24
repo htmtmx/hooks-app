a22f647859e8fe1a02ba613d56aca79857761819
Cesar Pineda <cesar.pineda@kodevox.com>
2025-01-24 11:04:06 -0600

    ✨ Add CounterApp component with state management using useState hook

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HooksApp } from './HooksApp'
+import { CounterApp } from './01_useState/CounterApp'
+// import { HooksApp } from './HooksApp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <HooksApp />
+    <CounterApp/>
   </StrictMode>,
 )
