518729e71552dcf5722118bc5cee56fdb9ac5f83
Cesar Pineda <cesar.pineda@kodevox.com>
2025-01-29 09:17:33 -0600

    ✨ Update main.jsx to use SimpleForm component instead of CounterWithCustomHook

--- src/main.jsx
+++ src/main.jsx
@@ -2,11 +2,12 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 // import { CounterApp } from './01_useState/CounterApp'
-import { CounterWithCustomHook } from './01_useState/CounterWithCustomHook'
 // import { HooksApp } from './HooksApp'
+// import { CounterWithCustomHook } from './01_useState/CounterWithCustomHook'
+import { SimpleForm } from './02_useEffect/SimpleForm'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <CounterWithCustomHook/>
-  </StrictMode>,
+//   <StrictMode>
+    <SimpleForm/>
+//   </StrictMode>,
 )
