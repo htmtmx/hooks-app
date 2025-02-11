7c30726743f8a8990dd3149b0d53eeb313d74230
htmtmx <cesar.hpp96@hotmail.com>
2025-02-10 22:17:15 -0600

    ✨ Replace FocusScreen component with Layout component in main entry file

--- src/main.jsx
+++ src/main.jsx
@@ -1,16 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { FocusScreen } from './04_useRef/FocusScreen'
 // import { CounterApp } from './01_useState/CounterApp'
 // import { HooksApp } from './HooksApp'
 // import { CounterWithCustomHook } from './01_useState/CounterWithCustomHook'
 // import { SimpleForm } from './02_useEffect/SimpleForm'
 // import { FormWithCustomHook } from './02_useEffect/FormWithCustomHook'
 // import { MultipleCustomHooks } from './03_examples/MultipleCustomHooks'
+// import { FocusScreen } from './04_useRef/FocusScreen'
+import { Layout } from './05_useLayoutEffect/LAyout'
+
 
 createRoot(document.getElementById('root')).render(
     //   <StrictMode>
-    <FocusScreen/>
+    <Layout/>
     //   </StrictMode>,
 )
