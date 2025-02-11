aff570c688c164b92fb3045ed429eb392ed9fc55
Cesar Pineda <cesar.pineda@kodevox.com>
2025-02-11 13:00:10 -0600

    ✨ Implement routing in MainApp component with HomePage, AboutPage, and LoginPage

--- src/09_useContext/MainApp.jsx
+++ src/09_useContext/MainApp.jsx
@@ -1,8 +1,20 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { HomePage } from "./HomePage";
+import { LoginPage } from "./LoginPage";
+import { AboutPage } from "./AboutPage";
+
 export const MainApp = () => {
-  return (
-      <>
-          <h1>Main App</h1>
-          <hr />
-      </>
-  )
+    return (
+        <>
+            <h1>Main App</h1>
+            <hr />
+            <Routes>
+                <Route path="/" element={<HomePage />}/>
+                <Route path="about" element={<AboutPage />} />
+                <Route path="login" element={<LoginPage />} />
+                {/* <Route path="/*" element={<h1>Not Found</h1>} /> */}
+                <Route path="/*" element= {<Navigate to="/about"/>}/>
+            </Routes>
+        </>
+    )
 }
