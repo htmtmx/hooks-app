f46a773c499740498a6fb83ab9dc3c0fa19cff70
htmtmx <cesar.hpp96@hotmail.com>
2025-02-10 21:04:28 -0600

    ✨ Add LoadingMessage component for improved loading state display

---
+++ src/03_examples/LoadingMessage.jsx
@@ -0,0 +1,7 @@
+export const LoadingMessage = () => {
+    return (
+      <section style={{height: 200}} className='alert alert-info d-flex w-100 text-center p-0 m-0 align-items-center justify-content-center'>
+          <h1>Loading ...</h1>
+      </section>
+  )
+}
