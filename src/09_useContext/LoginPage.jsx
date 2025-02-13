fd2281ae4eb74bab296783fd0da31cf42dd15a45
Cesar Pineda <cesar.pineda@kodevox.com>
2025-02-12 18:42:27 -0600

    ✨ Integrate UserContext in LoginPage for user state management

--- src/09_useContext/LoginPage.jsx
+++ src/09_useContext/LoginPage.jsx
@@ -1,4 +1,9 @@
+import { useContext } from "react"
+import { UserContext } from "./context/UserContext";
+
 export const LoginPage = () => {
+    const algo = useContext(UserContext);
+    console.log(algo);
   return (
       <>
           <h1>Login Page</h1>
